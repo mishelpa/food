72a6403924487b54e013429123e20a9b2dcafe66
mishelpa <judith.paredesartica@gmail.com>
2020-05-21 22:14:31 -0500

    ok

--- FoodPeruWeb/src/app/components/product/product.component.ts
+++ FoodPeruWeb/src/app/components/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../services/products/products.service';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: ActivatedRoute, private productsService: ProductsService) { }
 
   ngOnInit(): void {
+
   }
 
   getProductById(id) {
