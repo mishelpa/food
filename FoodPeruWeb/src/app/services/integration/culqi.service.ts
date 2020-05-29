ebb46c404bcee835818255fc5b3b25b2310c793a
YasmitDaysi <yasdayvasand98@mail.com>
2020-05-29 13:40:36 -0500

    getting token

--- FoodPeruWeb/src/app/services/integration/culqi.service.ts
+++ FoodPeruWeb/src/app/services/integration/culqi.service.ts
@@ -1,25 +1,53 @@
 import { Injectable } from '@angular/core';
-
+export declare let Culqi;
 
 @Injectable({
     providedIn: 'root'
   })
 
 export class CulqiService {
-
-    constructor() {
-    }
-    async getToken(){
-    console.log('aqui');
-    try {
-      await window.Culqi.createToken();
-      console.log(window.Culqi.token);
-    } catch (error) {
-      console.log(error);
-      
-    }
+  token_id: string;
    
-      
-
+  constructor() {
+    document.addEventListener ('payment_event', (token: any) => {
+    this.token_id = token.detail;
+    console.log(this.token_id);
+  });
+}
+    
+    initCulqi () {
+      Culqi.publicKey = "pk_test_WkOlOvuX4C3asoLB"
     }
+
+    payorder(description: string, amount: number) {
+      Culqi.settings ({
+        title: 'Culqi Store',
+        currency: 'PEN',
+        description: description,
+        amount: amount*100
+      });
+  
+      Culqi.options({
+        lang: 'auto',
+        modal: true,
+        installments: false,
+        customButton:"",
+    });
+    Culqi.open ();
+  
+    }
+
+    open() {
+      Culqi.open ();
+    }
+    // async getToken(){
+    // console.log('aqui');
+    // try {
+    //   await window.Culqi.createToken();
+    //   console.log(window.Culqi.token);
+    // } catch (error) {
+    //   console.log(error);
+      
+    // }
+    // }
 }
