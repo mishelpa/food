abf2cd9389d79744c9766695c56c979be576a9eb
mishelpa <judith.paredesartica@gmail.com>
2020-06-02 18:24:37 -0500

    add all changes

--- src/app/services/integration/culqi.service.ts
+++ src/app/services/integration/culqi.service.ts
@@ -33,6 +33,13 @@ export class CulqiService {
         modal: true,
         installments: false,
         customButton: '',
+        style: {
+          logo: 'https://culqi.com/LogoCulqi.png',
+          maincolor: '#FF5500',
+          buttontext: '#ffffff',
+          maintext: '#4A4A4A',
+          desctext: '#4A4A4A'
+        }
     });
       Culqi.open();
 
